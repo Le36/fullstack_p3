582653f017e1e183a504b4084df448ddc6a736e8
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-06 05:39:56 +0200

    3.18*: puhelinluettelo ja tietokanta, step6

--- index.js
+++ index.js
@@ -14,37 +14,16 @@ const cors = require('cors')
 
 app.use(cors())
 
-let persons = [
-    {
-        id: 1,
-        name: 'Arto Hellas',
-        number: '040-123456'
-    },
-    {
-        id: 2,
-        name: 'Ada Lovelace',
-        number: '39-44-5323523'
-    },
-    {
-        id: 3,
-        name: 'Dan Abramov',
-        number: '12-43-234345'
-    },
-    {
-        id: 4,
-        name: 'Mary Poppendieck',
-        number: '39-23-6423122'
-    }
-]
-
 app.get('/api/persons', (req, res, next) => {
     Person.find({}).then(persons => {
         res.json(persons)
     }).catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
-    res.send(`Phonebook has info for ${persons.length} persons <br> ${new Date()}`)
+app.get('/info', (req, res, next) => {
+    Person.find({}).then(persons => {
+        res.send(`Phonebook has info for ${persons.length} persons <br> ${new Date()}`)
+    }).catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
